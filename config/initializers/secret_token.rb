cbd10a97e2062da467940bf6d0976bea49025938
Brian Maissy <brian.maissy@gmail.com>
2011-10-09 15:59:18 -0700

    added rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WSMA::Application.config.secret_token = '56c932291a73b9fa0dd3f2dccc660e6aa8a92b34802f9bcb978da5f93a40f184c0e2a3547359fb391eda51034f21929f5ad4d7d62d1684b601096860e4131050'
