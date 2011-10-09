5d8ae3034af0dd63c9a27bf4f78d1c02d55d4174
Brian Maissy <brian.maissy@gmail.com>
2011-10-09 16:18:50 -0700

    created users scaffold, modified index.html

---
+++ db/migrate/20111009230645_create_users.rb
@@ -0,0 +1,18 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.string :hashed_password
+      t.string :salt
+      t.string :jom_social_id
+      t.integer :access_level
+      t.string :name
+      t.string :phone_number
+      t.string :room_number
+      t.decimal :hours_per_week
+      t.text :notes
+
+      t.timestamps
+    end
+  end
+end
