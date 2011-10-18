41a555f0dd04f4df3ac6b2e7ed580f6caa85b724
Brian Maissy <brian.maissy@gmail.com>
2011-10-18 02:39:11 -0700

    changed naming for some encrypted connection unit tests

--- test/unit/encrypted_connection_test.rb
+++ test/unit/encrypted_connection_test.rb
@@ -1,12 +1,12 @@
 require 'test_helper'
 
 class EncryptedConnectionTest < ActiveSupport::TestCase
-  test "encrypted_connection_set_defaults_works" do
+  test "set_defaults_works" do
     @connection = EncryptedConnection.new
     assert @connection.valid?
   end
   
-  test "encrypted_connection_does_not_change_by_finding_it" do
+  test "does_not_change_by_finding_it" do
     @connection = encrypted_connections(:one)
     assert @connection == EncryptedConnection.find_by_public_key(@connection.public_key)
   end
