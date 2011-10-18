79128a8c1e779b2262a82e1e13192a029029b714
Brian Maissy <brian.maissy@gmail.com>
2011-10-18 05:24:52 -0700

    reformatted encrypted connection unit tests

--- test/unit/encrypted_connection_test.rb
+++ test/unit/encrypted_connection_test.rb
@@ -1,17 +1,17 @@
 require 'test_helper'
 
 class EncryptedConnectionTest < ActiveSupport::TestCase
-  test "set_defaults_works" do
-    @connection = EncryptedConnection.new
-    assert @connection.valid?
+  test "set defaults works" do
+    connection = EncryptedConnection.new
+    assert connection.valid?
   end
   
-  test "does_not_change_by_finding_it" do
-    @connection = encrypted_connections(:one)
-    assert @connection == EncryptedConnection.find_by_public_key(@connection.public_key)
+  test "does not change by finding it" do
+    connection = encrypted_connections(:one)
+    assert_equal connection, EncryptedConnection.find_by_public_key(connection.public_key)
   end
   
-  test "public_private_key_pair_work_properly" do
+  test "public and private key pair work properly" do
     #TODO: encrypt something and then decrypt it
     assert false
   end
