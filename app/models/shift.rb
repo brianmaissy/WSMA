33706a0c90649eac4cb6bfa9c9752198ded1b8a6
Benjamin Lui <benlui@berkeley.edu>
2011-10-19 22:06:01 -0700

    shift and assignment validation, started writing tests for chores

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -1,2 +1,21 @@
 class Shift < ActiveRecord::Base
+  
+  belongs_to :chore
+  belongs_to :user
+  has_many :assignments
+  
+  validates_presence_of :day_of_week, :time, :temporary
+  validates_numericality_of :day_of_week :greater_than_or_equal_to => 0, :less_than_or_equal_to => 7
+  #TODO: find a way to do time validation
+  #validates_time :time
+  validate :temporary_has_legal_value
+  
+  def temporary_has_legal_value
+    errors.add(:temporary, 'must be 0 or 1') if not [0, 1].include? temporary
+  end
+  
+  def intiailize(chore, day_of_week, time, temporary)
+    #TODO:implement this
+  end
+  
 end
