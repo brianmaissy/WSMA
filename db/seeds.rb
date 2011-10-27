9772598190c52da41ad15328d2298ce1b68c97e4
Brian Maissy <brian.maissy@gmail.com>
2011-10-26 22:39:30 -0700

    added bootstrap task and some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
+
+houses = House.create([{:name => 'Griffindor'}, {:name => 'Hufflepuff'}, {:name => 'Ravenclaw'}, {:name => 'Slytherin'}])
+harry = User.create(:house => houses[0], :name => 'Harry Potter', :access_level => 1, :email => 'a', :hashed_password => 'a', :salt => 'a')
+ron = User.create(:house => houses[0], :name => 'Ron Weasley', :access_level => 1, :email => 'b', :hashed_password => 'b', :salt => 'b')
+malfoy = User.create(:house => houses[3], :name => 'Draco Malfoy', :access_level => 1, :email => 'c', :hashed_password => 'c', :salt => 'c')
