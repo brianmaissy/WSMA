09f900cf6d3a085c493e8ece8fa56ace9a5e7007
Brian Maissy <brian.maissy@gmail.com>
2011-10-31 01:45:21 -0700

    linked shift to chore properly

--- db/migrate/20111014223039_create_shifts.rb
+++ db/migrate/20111014223039_create_shifts.rb
@@ -1,6 +1,7 @@
 class CreateShifts < ActiveRecord::Migration
   def change
     create_table :shifts do |t|
+      t.integer :chore_id
       t.integer :day_of_week
       t.time :time
       t.integer :temporary
