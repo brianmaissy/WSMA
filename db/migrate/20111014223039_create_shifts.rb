396567ff0f671c6c342eb6bb3d1c5cbe2eb423dc
Brian Maissy <brian.maissy@gmail.com>
2011-10-31 02:40:20 -0700

    properly linked shifts to users

--- db/migrate/20111014223039_create_shifts.rb
+++ db/migrate/20111014223039_create_shifts.rb
@@ -1,7 +1,8 @@
 class CreateShifts < ActiveRecord::Migration
   def change
     create_table :shifts do |t|
-      t.integer :chore_id
+      t.integer :chore_id, :null => false
+      t.integer :user_id
       t.integer :day_of_week
       t.time :time
       t.integer :temporary
