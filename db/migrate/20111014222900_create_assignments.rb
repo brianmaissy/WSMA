7bc515f797f2d07d49550aae5a21a189c4ac23ac
Brian Maissy <brian.maissy@gmail.com>
2011-10-31 02:29:10 -0700

    restricted assignment's fks to be not null

--- db/migrate/20111014222900_create_assignments.rb
+++ db/migrate/20111014222900_create_assignments.rb
@@ -1,8 +1,8 @@
 class CreateAssignments < ActiveRecord::Migration
   def change
     create_table :assignments do |t|
-      t.integer :user_id
-      t.integer :shift_id
+      t.integer :user_id, :null => false
+      t.integer :shift_id, :null => false
       t.integer :week
       t.integer :status
       t.string :blow_off_job_id
