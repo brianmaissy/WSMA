41ac3646f30764fa9b8e803c89483c9fdc8dc8b7
Brian Maissy <brian.maissy@gmail.com>
2011-10-31 00:07:13 -0700

    Moved authentication logic out into application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,4 +23,19 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def authorize_admin
+    unless @logged_user.access_level >= 2
+      session[:original_uri] = request.url
+      flash[:notice] = "Please log in to access administrative area"
+      redirect_to :controller => :users, :action => :login
+    end
+  end
+
+  def authorize_user
+    unless @logged_user.access_level >= 2 or (@logged_user.access_level == 1 and params[:id].to_i == @logged_user.id)
+      flash[:notice] = "You do not have the privileges to see another user's page"
+        redirect_to(:action => "show", :id => @logged_user.id)
+    end
+  end
+
 end
