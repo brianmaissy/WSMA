67f6734a008f525657412fd69f4d9af921048964
Brian Maissy <brian.maissy@gmail.com>
2011-11-22 00:04:34 -0800

    updated README and seeds.rb in preparation for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
 
-houses = House.create([{:name => 'Griffindor'}, {:name => 'Hufflepuff'}, {:name => 'Ravenclaw'}, {:name => 'Slytherin'}])
-harry = User.create(:house => houses[0], :name => 'Harry Potter', :access_level => 1, :email => 'a', :password => 'seeker')
-ron = User.create(:house => houses[0], :name => 'Ron Weasley', :access_level => 1, :email => 'b', :password => 'hermione')
-malfoy = User.create(:house => houses[3], :name => 'Draco Malfoy', :access_level => 1, :email => 'c', :password => 'voldemort')
-admin = User.create(:house => houses[2], :name => 'admin', :access_level => 3, :email => 'admin', :password => 'admin')
+testHouse = House.create(:name => 'Test House')
+admin = User.create(:house => testHouse, :name => 'admin', :access_level => 3, :email => 'admin', :password => '169testers')
+wsm = User.create(:house => testHouse, :name => 'wsm', :access_level => 2, :email => 'wsm', :password => '169testers')
+user = User.create(:house => testHouse, :name => 'user', :access_level => 1, :email => 'user', :password => '169testers')
+
