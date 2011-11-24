90c5b5cfbf6c5177aa5e72bed0a5c3e9b5a074a1
Brian Maissy <brian.maissy@gmail.com>
2011-11-23 22:37:36 -0800

    Revert "forgot to commit this earlier, just the new schema.rb after removing the job_id from the migration"
    This reverts commit ec1d912e0473ede6291e84f21ae87dd93ffc7dcc.

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,12 +1,5 @@
 class DemoController < ApplicationController
 
-  def advance_time
-    #TimeProvider.advance_mock_time(params[:hours].hours)
-    flash[:notice] = "Time advanced by #{params[:hours]} hours"
-    respond_to do |format|
-      format.js
-    end
-  end
   
   # GET /users
   # GET /users.json
